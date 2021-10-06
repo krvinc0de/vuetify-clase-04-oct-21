a59266dba1af628af5bed8af57547b7cfee716d3
Kevin <polopolopolopoline@gmail.com>
2021-10-06 11:07:34 -0500

    Rutas nuevas
    Se agregaron nuevas rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Crear from '../components/Crear.vue'
+import Editar from '../components/Editar.vue'
+import Listar from '../components/Listar.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,19 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/Crear',
+    name: 'Crear',
+    component: Crear
+  },
+  {
+    path: '/Editar',
+    name: 'Editar',
+    component: Editar
+  },
+  {
+    path: '/Listar',
+    name: 'Listar',
+    component: Listar
   }
 ]
 
