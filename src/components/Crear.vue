51690e8e0cce606fa1778db6949785980660cfdc
Kevin <polopolopolopoline@gmail.com>
2021-10-06 21:35:02 -0500

    creacion de vista crear

---
+++ src/components/Crear.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="pa-2 ma-2">
+        <div class="text-h4">
+            Template Crear
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
