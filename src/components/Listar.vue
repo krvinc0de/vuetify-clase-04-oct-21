c3f7fdc3b4ce5677529265d3bcf87cca4458a369
Kevin <polopolopolopoline@gmail.com>
2021-10-06 21:35:25 -0500

    creacion de vista listar

---
+++ src/components/Listar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="pa-2 ma-2">
+        <div class="text-h4">
+            Usuarios del sistema
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    created: function(){
+
+    },
+    methods: {
+        consultarUsuarios(){
+            //sirve para ir a links en servidores web fetch
+            fetch('http://localhost/usuarios/')
+            .then(respuesta => respuesta.json())
+            .then( (datos) => {
+                console.log(datos)
+            })
+            .catch(console.log)
+        }
+    }
+}
+</script>
