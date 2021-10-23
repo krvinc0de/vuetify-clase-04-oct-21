b644c1e599ea362405ac054f23b74648723ba89c
Kevin <polopolopolopoline@gmail.com>
2021-10-23 02:09:18 -0500

    Nuevo Componente
    Se creo componente footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    <footer class="footer-basic text-center">
+        <p class="copyright">Todos los derechos reservados © 2021</p>
+        <p class="font-weight-light">ermkgjerngermgmergjklm</p>
+    </footer>
+    </div>
+</template>
+
+<style scoped>
+.footer-basic{
+    color:gainsboro;
+    background-color: #363636;
+    width: 100%;
+    height: 120px;
+    padding-top: 30px;
+    margin-top: 10px;
+   
+}
+</style>
